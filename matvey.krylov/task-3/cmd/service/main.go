f743bc7b27d8025073531a22687f33d3590e2fef
mkryloff <matwej71@gmail.com>
2025-10-31 23:55:09 +0300

    [TASK-3] first implementation probably finished

--- matvey.krylov/task-3/cmd/service/main.go
+++ matvey.krylov/task-3/cmd/service/main.go
@@ -1,3 +1,35 @@
 package main
 
-func main() {}
+import (
+	"flag"
+
+	"github.com/mkryloff/task-3/internal/ioutils"
+	"github.com/mkryloff/task-3/internal/parser"
+	"github.com/mkryloff/task-3/internal/processor"
+)
+
+func main() {
+	configPath := flag.String("config", "", "Path to YAML configuration file")
+	flag.Parse()
+
+	if *configPath == "" {
+		panic("configuration file path must be provided using --config flag")
+	}
+
+	cfg, err := ioutils.LoadConfiguration(*configPath)
+	if err != nil {
+		panic("failed to load configuration: " + err.Error())
+	}
+
+	currencyData, err := parser.ParseCurrencyData(cfg.InputFile)
+	if err != nil {
+		panic("failed to parse currency data: " + err.Error())
+	}
+
+	sortedCurrencies := processor.SortCurrenciesByValue(currencyData.Valutes)
+
+	err = ioutils.WriteJSONOutput(sortedCurrencies, cfg.OutputFile)
+	if err != nil {
+		panic("failed to write output file: " + err.Error())
+	}
+}
